c790db70b24aee4474b38a08550871efa5a3f8c4
Alexandr Vagner <alexmednogorsk@mail.ru>
2020-03-24 00:19:09 +0500

    Add local settings

--- main/settings.py
+++ main/settings.py
@@ -121,3 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+try:
+    from .local_settings import *  # noqa
+except ImportError:
+    pass
