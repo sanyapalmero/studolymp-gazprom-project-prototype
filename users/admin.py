3079834d26378bda67458e93f74e69b0aa57837d
Alexandr Vagner <alexmednogorsk@mail.ru>
2020-03-28 19:11:48 +0500

    Add employer field to User model

--- users/admin.py
+++ users/admin.py
@@ -1,10 +1,25 @@
 from django.contrib import admin, auth
+from django.contrib.auth.admin import UserAdmin
 
 from .models import User
 
 admin.site.unregister(auth.models.Group)
 
 
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('email', 'username', 'role', 'created_at')
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email', 'username', 'role', 'employer', 'created_at')
+    search_fields = ('email', 'username', 'role')
+    readonly_fields = ('created_at', 'last_login')
+
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        """Return users with role=ROLE_EMPLOYER for employer dropdown"""
+        if db_field.name == "employer":
+            kwargs["queryset"] = User.objects.filter(role=User.ROLE_EMPLOYER)
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+admin.site.register(User, CustomUserAdmin)
--- users/models.py
+++ users/models.py
@@ -39,14 +39,28 @@ class User(AbstractBaseUser):
 
     email = models.EmailField(unique=True, max_length=255, verbose_name="Email")
     username = models.CharField(unique=False, max_length=255, verbose_name="Имя")
-    role = models.CharField(max_length=64, choices=ROLE_CHOICES, default=ROLE_EMPLOYEE)
-    created_at = models.DateTimeField(auto_now_add=True)
+    role = models.CharField(max_length=64, choices=ROLE_CHOICES, default=ROLE_EMPLOYEE, verbose_name="Роль")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+
+    # This field for users with role=ROLE_EMPLOYEE, for saving employer.
+    employer = models.ForeignKey(
+        "self", on_delete=models.SET_NULL, null=True, blank=True,
+        related_name="employees", verbose_name="Руководитель"
+    )
 
     objects = UserManager()
 
     @property
     def is_admin(self):
         return self.role == self.ROLE_ADMIN
+
+    @property
+    def is_employer(self):
+        return self.role == self.ROLE_EMPLOYER
+
+    @property
+    def is_employee(self):
+        return self.role == self.ROLE_EMPLOYEE
 
     @property
     def is_user(self):
@@ -61,3 +75,7 @@ class User(AbstractBaseUser):
 
     def has_module_perms(self, app_label):
         return self.is_admin
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
