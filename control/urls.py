a551327d4d1c2774ec94c65225140ed1b28fc703
Alexandr Vagner <alexmednogorsk@mail.ru>
2020-03-28 21:02:26 +0500

    Add employees table

--- control/urls.py
+++ control/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'control'
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
+    path('employees/', views.EmployessView.as_view(), name='employees'),
 ]
--- control/views.py
+++ control/views.py
@@ -1,5 +1,20 @@
+from django.utils.decorators import method_decorator
 from django.views import generic
+
+from users.decorators import login_required
+from users.models import User
 
 
 class HomeView(generic.TemplateView):
     template_name = 'control/home.html'
+
+
+@method_decorator(login_required, name='dispatch')
+class EmployessView(generic.ListView):
+    template_name = 'control/employees.html'
+    model = User
+    paginate_by = 20
+
+    def get_queryset(self):
+        queryset = self.model.objects.filter(role=User.ROLE_EMPLOYEE, employer=self.request.user)
+        return queryset
